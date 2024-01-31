4676bcb822683b8b60eec292543fcbfb9506e661
pykidev <96683306+pykidev@users.noreply.github.com>
2024-01-31 13:34:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ app.post('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, '0.0.0.0' () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server is listening on port ${port}`);
 });
